cd4683c745d44c8055a80b98f26bed09615c2761
Antonio Avolio <antonioavolio18@gmail.com>
2022-12-13 17:20:52 +0100

    Changed to stage mechanic with build 2.0
    Signed-off-by: Antonio Avolio <antonioavolio18@gmail.com>

--- Vampire's Hunt/GameScene.swift
+++ Vampire's Hunt/GameScene.swift
@@ -11,26 +11,17 @@ import GameKit
 
 class GameScene: SKScene {
     private let baseSpeed : CGFloat = -350
-    private let background = SKSpriteNode(imageNamed: "Background")
     private let musicAudioNode = SKAudioNode(fileNamed: "backgroundMusic.mp3")
     private var player = Player()
+    private var stage : Int = 1
     private var lives : Int = 3
     private var blood : Int = 0
     private var clock : TimeInterval = 0.0
     private var coinvalue : SKLabelNode?
     private var citizen = Citizen(citizenType: Citizen.CitizenType.citizen1)
-    private var typeOfCivil = ["citizen1", "citizen2", "virgin"]
+    private var dropSpeed : CGFloat = 0.0
+    private var scalingFactor : CGFloat = 0.75
     
-    var scalingFactor : CGFloat {
-        return pow(0.75, clock/10.0)
-        
-    }
-    var dropSpeed : CGFloat {
-        // divided by 10 for unit every 10s
-        // divided by 100 to get 0.01 decimal point
-        // multiplied by 5 to decrese faster
-        return  ((0.005 * baseSpeed * clock)).rounded()
-    }
     
     override func didMove(to view: SKView) {
         audioEngine.mainMixerNode.outputVolume = 0.0
@@ -44,33 +35,59 @@ class GameScene: SKScene {
         }
         physicsWorld.gravity = .zero
         physicsWorld.contactDelegate = self
+        changeBackground()
+        addMenu()
+        setClock()
+        addPlayer()
+        startSpawn()
+        changeLevel()
+    }
+    private func changeBackground(){
+        //let background = SKSpriteNode(imageNamed: "background\(stage)")
+        let background = SKSpriteNode(imageNamed: "background1")
         background.name = "background"
         background.anchorPoint = CGPoint(x: 0, y: 0)
         background.zPosition = Layer.background.rawValue
         background.position = CGPoint(x: 0, y: 0)
         addChild(background)
-        addMenu()
-        setClock()
-        addPlayer()
-        startSpawn()
     }
-    
+    private func changeLevel(){
+        if(stage < 100){ // Last stage
+            //MARK: remove spawned citizen
+            run(SKAction.sequence([
+                SKAction.wait(forDuration:20),
+                SKAction.run {
+                    self.removeAllActions()
+                    self.removeChildren(in: [self.childNode(withName: "background")!])
+                    self.removeChildren(in: [self.childNode(withName: "clock")!])
+                },
+                SKAction.run {
+                    self.startSpawn()
+                    self.changeDiff()
+                    self.setClock()
+                },
+                SKAction.run {
+                    self.changeLevel()
+                }
+            ]))
+        }
+        
+    }
     
     private func startSpawn(){
-        spawnProjectile()
+        spawnMultipleProjectile()
         spawnMultipleCitizen()
     }
-    private func spawnProjectile(){
-        // MARK: Not updating scalingFactor beacuse of one time call
-       
-        run(SKAction.repeatForever(
-            SKAction.sequence([
-                SKAction.run(addProjectile),
-                SKAction.wait(forDuration: 2.0 * scalingFactor,withRange: 3.5 * scalingFactor)
-            ])
-        ))
-    }
     
+    func spawnMultipleProjectile(){
+        let wait = SKAction.wait(forDuration: 1.0 * scalingFactor , withRange: 1.5 * scalingFactor)
+        let spawn = SKAction.run {
+            self.spawnProjectile()
+        }
+        let sequence = SKAction.sequence([wait, spawn])
+        let repeatAction = SKAction.repeat(sequence, count: 10000)
+        run(repeatAction, withKey: "projectile")
+    }
     
     func spawnCitizen(citizenType: Citizen.CitizenType){
         citizen = Citizen(citizenType: citizenType)
@@ -85,7 +102,7 @@ class GameScene: SKScene {
     func spawnMultipleCitizen(){
         let wait = SKAction.wait(forDuration: TimeInterval(2.0), withRange: TimeInterval(1.5))
         let spawn = SKAction.run {
-            let type = weightedRandomCitizen(phase: Int(self.clock/10))
+            let type = weightedRandomCitizen(phase: self.stage)
             self.spawnCitizen(citizenType: type)
         }
         let sequence = SKAction.sequence([wait, spawn])
@@ -96,6 +113,7 @@ class GameScene: SKScene {
     private func setClock() {
         let clockLabel = SKLabelNode()
         let dateFormatter = DateComponentsFormatter()
+        clockLabel.name = "clock"
         clockLabel.text = dateFormatter.string(from: self.clock)!
         clockLabel.fontSize = CGFloat(26)
         clockLabel.zPosition = Layer.ui.rawValue
@@ -118,7 +136,7 @@ class GameScene: SKScene {
         player.walk()
     }
     
-    private func addProjectile() {
+    private func spawnProjectile() {
         var projectile = Arrow(arrowType: Arrow.ArrowType.arrow)
         let actualX = random(min: size.width * 0.05, max: size.width * 0.95)
         setupPhysics(node: &projectile, categoryBitMask: PhysicsCategory.arrow, contactTestBitMask: PhysicsCategory.player)
@@ -208,6 +226,12 @@ class GameScene: SKScene {
     }
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         player.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+    }
+    private func changeDiff(){
+        self.stage += 1
+        self.scalingFactor = pow(0.8, CGFloat(self.stage))
+        self.dropSpeed = ((0.05 * baseSpeed * CGFloat(self.stage))).rounded()
+        changeBackground()
     }
 }
 
